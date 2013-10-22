ac77b33e0db517efa2a2f14626603d1c11df1a64
root <root@testbed1.abs.us.corp>
2013-10-22 16:47:05 -0400

    Further refinement to pytest5

--- pytest5.py
+++ pytest5.py
@@ -23,7 +23,7 @@ class InputFile:
 			for line in open(self.path):
 				if ( len(line) > 0 ) and ( line.count(',') == 12 ):
 					line = re.sub(rm_trailcomma,'',line)
-					yield line
+					yield line	# return a generator object!
 				else:
 					continue
 		except:
@@ -35,31 +35,61 @@ class PerfObj:
 	get_LD = re.compile(',Logical Drive (.*?),')
 	def __init__(self, u_input, verbose=False):
 		lu_input = u_input.split(',', 11)
-		dictout = {}
+		self.dictout = {}
 		if verbose:
 			print('VERBOSE: Input on PerfObj constructor is: ' + str(u_input) + ' : .')
 		if len(lu_input) == 12:
-			dictout['system_name'] = str(u_input[0])
-			dictout['timestamp'] = datetime.strptime(lu_input[1])
-			dictout['IPaddr_controllerA'] = str(u_input[2])
-			dictout['IPaddr_controllerB'] = str(u_input[3])		
-			if get_controller.search(u_input[4]):
-				dictout['controllerTotal'] = get_controller.search(inp).group(1)
-				dictout['LogicalDriveName'] = None
+			self.dictout['system_name'] = str(u_input[0])
+			self.dictout['timestamp'] = datetime.strptime(lu_input[1])
+			self.dictout['IPaddr_controllerA'] = str(u_input[2])
+			self.dictout['IPaddr_controllerB'] = str(u_input[3])		
+			if ( get_controller.search(u_input[4]) ) and ( get_LD.search(u_input[4]) ):
+				raise ValueError(' : unexpected input of ' + str(u_input[4]) + ' for controller or logical drive field #5 to constructor on class \'PerfObj\' - both controller and logical drive found where only one expected')
+			elif get_controller.search(u_input[4]):
+				self.dictout['controllerTotal'] = get_controller.search(inp).group(1)
+				self.dictout['LogicalDriveName'] = None
 			elif get_LD.search(u_input[4]):
-				dictout['controllerTotal'] = None
-				dictout['LogicalDriveName'] = get_LD.search(inp).group(1)
+				self.dictout['controllerTotal'] = None
+				self.dictout['LogicalDriveName'] = get_LD.search(inp).group(1)
 			else:
 				raise ValueError(' : unexpected input of ' + str(u_input[4]) + ' for controller or logical drive field #5 to constructor on class \'PerfObj\'')
-			dictout['total_IOPS'] = int(u_input[5])
-			dictout['read_percent'] = float(u_input[6])
-			dictout['cache-hit_percent'] = float(u_input[7])
-			dictout['current_KBsec'] = float(u_input[8])
-			dictout['max_KBsec'] = float(u_input[9])
-			dictout['current_IOPS'] = float(u_input[10])
-			dictout['max_IOPS'] = float(u_input[11])
+				return None
+			self.dictout['total_IOPS'] = int(u_input[5])
+			self.dictout['read_percent'] = float(u_input[6])
+			self.dictout['cache-hit_percent'] = float(u_input[7])
+			self.dictout['current_KBsec'] = float(u_input[8])
+			self.dictout['max_KBsec'] = float(u_input[9])
+			self.dictout['current_IOPS'] = float(u_input[10])
+			self.dictout['max_IOPS'] = float(u_input[11])
 			if verbose:
 				print('VERBOSE: Output on PerfObj constructor is: ' + str(dictout) + ' : .')
-			return dictout
+			return self.dictout
 		else:
 			raise AttributeError(' : Found ' + str(len(lu_input)) + ' fields in input string:\n' + u_input + '\n... where 12 expected on class \'PerfObj\', constructor method.  Invalid input.')
+			return None
+	def SingleDBInsert:
+
+class PerfGroup:
+	def __init__(self, whichdb):
+		if len(self.output) == 0:
+			self.output = ()
+			return True
+		else:
+			raise "Warning: calling constructor on non-empty object from class PerfGroup.  No values have been added or removed."
+			return False
+	def Add(dict_in):
+		if isinstance(dict_in, dict):
+			
+		else:
+			raise ValueError(' : invalid input - method \'Add\' on class PerfGroup expects input as dictionary')
+	def DeDup:
+		if len(self.output > 0):
+			
+		else:
+			raise ValueError(' : nothing to deduplicate')
+	def MkCSV:
+		if len(self.output > 0):
+		
+		else:
+			raise ValueError(' : nothing to output.')
+	def DBInsert:
