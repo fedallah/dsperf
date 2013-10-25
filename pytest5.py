f7d8450c3d65e4d50150639a438a4579045a770e
root <root@testbed1.abs.us.corp>
2013-10-25 17:03:17 -0400

    Schema class, work on databasing class.

--- pytest5.py
+++ pytest5.py
@@ -5,52 +5,6 @@ import re
 from datetime import datetime
 from hashlib import sha256
 
-def globalSchemaInit(name = False, type = False):
-	"""
-	Provide a simple and quick way to keep dict and database table schemas all in one location.
-	"""
-	schemaMap = {}								# global schema with attached SQL datatypes to be used across all classes.  This is immutable and should only ever be stored here!
-	schemaMap['system_name'] = 'TEXT'
-	schemaMap['timestamp'] 	= 'DATETIME'
-	schemaMap['IPaddr_controllerA'] = 'TEXT'
-	schemaMap['IPaddr_controllerB'] = 'TEXT'
-	schemaMap['controllerTotal'] = 'TEXT'
-	schemaMap['LogicalDriveName'] = 'TEXT'
-	schemaMap['total_IOPS'] = 'INT'
-	schemaMap['read_percent'] = 'FLOAT'
-	schemaMap['cache-hit_percent'] = 'FLOAT'
-	schemaMap['current_KBsec'] = 'FLOAT'
-	schemaMap['max_KBsec'] = 'FLOAT'
-	schemaMap['current_IOPS'] = 'FLOAT'
-	schemaMap['max_IOPS'] = 'FLOAT'
-	schemaMap['pkey_sha256hex'] = 'CHAR(64)'	# SHA256 primary key for use with nonvolatile RMDBS
-	if schemaMap:
-		if name and type:
-			return tuple(schemaMap.values()), tuple(schemaMap.keys())
-		elif name:
-			return tuple(schemaMap.keys())
-		elif type:
-			return tuple(schemaMap.values())
-		else:
-			return tuple(schemaMap.keys())	# default - return keys only if caller does not specify
-	else:
-		raise ValueError(' : no schema defined, nothing to output.')
-		return False
-
-globalSchema = globalSchemaInit(True, False)
-tableSchema = globalSchemaInit(False, True)
-
-def globalDictInputChk(dictInput, tableSchema = globalSchema):
-	"""
-	Check for valid or invalid input: is our input a dictionary?  Is it congruent with provided tableSchema (default globalSchema)?
-	This function should be valid for use in class PerfObj and elsewhere.
-	"""
-	if ( isinstance(dictInput, dict) and ( list(dictInput.keys()).sort() == list(tableSchema).sort() ) ):
-		return True
-	else:
-		raise ValueError(' : input ' + str(dictInput) + ' is either not a dictionary or has keys that do not comply with the enforced schema.')
-		return False
-
 def globalVerbose(isOn, v_input):
 	"""
 	Provide an easy interface to print method input on demand
@@ -74,6 +28,36 @@ def mkCSV(origIter):
 			allElems = (allElems + ',' + '\"' + str(elem) + '\"')
 	return allElems	
 
+class Schema:
+	"""
+	Provides an interface to define, export, and verify the correct schema for our input program-wide.  Dictionary keys are
+	both key names for class PerfObj and column names for a relational database.  Values are SQL-compatible data types (e.g.
+	"TEXT", "CHAR(64)", "FLOAT", "INT", et al.
+	"""
+	def __init__(self, schemaMapIn):
+		# Construct; accept a dictionary object.
+		if isinstance(schemaMapIn, dict):
+			self.schemaMap = schemaMapIn
+			return None
+		else:
+			raise ValueError(' : tried to pass something other than a dictionary object where a dictionary is expected.')
+			return False
+	def DictInputChk(self, dictInput):
+		# Validate that input is a dictionary and that keys match with self, return bool accordingly
+		if (isInstance(dictInput), dict) and (dictInput.keys() == self.schemaMap.keys()):
+			return True
+		else:
+			return False
+	def DictOut(self):
+		# return input dictionary
+		return self.schemaMap
+	def Titles(self):
+		# return a tuple of input keys
+		return tuple(self.schemaMap.keys())
+	def Types(self):
+		# return a tuple of output SQL-compatible types
+		return tuple(self.schemaMap.values())
+	
 class InputFile:
 	"""
 	InputFile ingests a standard comma-delimited file produced by smcli.  It helps to sanitize and verify the file, and 
@@ -86,7 +70,7 @@ class InputFile:
 		"""
 		self.path = path
 		try:
-			first_val = (open(self.path), 'U').readline()
+			first_val = (open(self.path), 'Ur').readline()
 			if first_val.count(',') != 12:
 				raise AttributeError(' : Found ' + str(len(isval)) + ' fields in first line of input file at \"' + str(self.path) + '\" where 12 expected.  This does not look like a valid file.')
 				return False
@@ -98,18 +82,21 @@ class InputFile:
 			return False
 	def Dump(self):
 		"""
-		Validate each line of input and remove a trailing comma.  Return a generator object.
+		Validate each line of input and remove a trailing comma.  Make a SHA256 hash of the plaintext string.
+		Split the string into a list, append the aforementioned SHA256 hash, then convert the list into a tuple.
 		"""
 		try:
-			for line in open(self.path, 'Urb'):
-				if ( len(line) > 0 ) and ( line.count(',') == 12 ):
-					line = str(line, encoding='UTF-8')
-					line = re.sub(rm_trailcomma,'',line)
-					line = line.split(',', 11)
-					line 
-					yield line	# return a generator object!
-				else:
-					continue
+			with open(self.path, 'Ur', encoding='utf-8') as inputfile:
+				for line in inputfile:
+					if ( len(line) > 0 ) and ( line.count(',') == 12 ):
+						hashme = sha256()	# init sha256sum method from hashlib
+						line = re.sub(rm_trailcomma,'',line)
+						hashme.update(line.encode(inputfile.encoding))
+						line = line.split(',', 11)
+						line.append(str(hashme.hexdigest()))
+						yield tuple(line) # return a generator object!
+					else:
+						continue
 		except:
 			raise IOError(' : could not open file on class InputFile for method \'Dump\' at location \"' + str(self.path) + '\".  Aborting.')
 			return False
@@ -123,8 +110,7 @@ class PerfObj:
 	# get_controller and get_LD: regex 'class variables' instatiated for the use of every object; they are used by constructor
 	get_controller = re.compile(',CONTROLLER IN SLOT (\w{1}),')
 	get_LD = re.compile(',Logical Drive (.*?),')
-	sha256worker = sha256()		# instatiate SHA256 hashing object
-	def __init__(self, u_input, tableSchema = globalSchema, verbose = False, verbose_func = globalVerbose):
+	def __init__(self, u_input, validSchema, verbose = False, verbose_func = globalVerbose):
 		"""
 		Validate and construct.
 		"""
@@ -133,30 +119,30 @@ class PerfObj:
 			if len(u_input) == 12:
 				self.dictout = {}
 				# map input to dictionary.
-				self.dictout[tableSchema[0]] = str(u_input[0])
-				self.dictout[tableSchema[1]] = datetime.strptime(u_input[1])
-				self.dictout[tableSchema[2]] = str(u_input[2])
-				self.dictout[tableSchema[3]] = str(u_input[3])		
+				self.dictout[validSchema[0]] = str(u_input[0])
+				self.dictout[validSchema[1]] = datetime.strptime(u_input[1])
+				self.dictout[validSchema[2]] = str(u_input[2])
+				self.dictout[validSchema[3]] = str(u_input[3])		
 				if ( get_controller.search(u_input[4]) ) and ( get_LD.search(u_input[4]) ):		# is this input for a controller or a logical drive?  It cannot be for both.
 					raise ValueError(' : unexpected input of ' + str(u_input[4]) + ' for controller or logical drive field #5 to constructor on class \'PerfObj\' - both controller and logical drive found where only one expected')
 					return False
 				elif get_controller.search(u_input[4]):
-					self.dictout[tableSchema[4]] = str(get_controller.search(u_input).group(1))
-					self.dictout[tableSchema[5]] = None
+					self.dictout[validSchema[4]] = str(get_controller.search(u_input).group(1))
+					self.dictout[validSchema[5]] = None
 				elif get_LD.search(u_input[4]):
-					self.dictout[tableSchema[4]] = None
-					self.dictout[tableSchema[5]] = str(get_LD.search(u_input).group(1))
+					self.dictout[validSchema[4]] = None
+					self.dictout[validSchema[5]] = str(get_LD.search(u_input).group(1))
 				else:
 					raise ValueError(' : unexpected input of ' + str(u_input[4]) + ' for controller or logical drive field #5 to constructor on class \'PerfObj\'')
 					return False
-				self.dictout[tableSchema[6]] = int(u_input[6])
-				self.dictout[tableSchema[7]] = float(u_input[7])
-				self.dictout[tableSchema[8]] = float(u_input[8])
-				self.dictout[tableSchema[9]] = float(u_input[9])
-				self.dictout[tableSchema[10]] = float(u_input[10])
-				self.dictout[tableSchema[11]] = float(u_input[11])
-				self.dictout[tableSchema[12]] = float(u_input[12])
-				self.dictout[tableSchema[13]] = 
+				self.dictout[validSchema[6]] = int(u_input[6])
+				self.dictout[validSchema[7]] = float(u_input[7])
+				self.dictout[validSchema[8]] = float(u_input[8])
+				self.dictout[validSchema[9]] = float(u_input[9])
+				self.dictout[validSchema[10]] = float(u_input[10])
+				self.dictout[validSchema[11]] = float(u_input[11])
+				self.dictout[validSchema[12]] = float(u_input[12])
+				self.dictout[validSchema[13]] = str(u_input[13])
 				return None	# on successful construction
 			else:
 				raise AttributeError(' : Found ' + str(len(u_input)) + ' fields in input string:\n' + u_input + '\n... where 12 expected on class \'PerfObj\', constructor method.  Invalid input.')
@@ -164,55 +150,89 @@ class PerfObj:
 		else:
 			raise ValueError(' : class PerfObj received non-tuple input on constructor where tuple expected; cannot continue.')
 			return False
-	def CSVOut(self, genCSVfunc = mkCSV, dictInput = self.dictout, tableSchema = globalSchema, inputChk = globalDictInputChk, printSchema = False, verbose_func = globalVerbose, verbose = False):
+	def CSVOut(self, genCSVfunc = mkCSV, dictInput = self.dictout, validSchema, inputChk = globalDictInputChk, printSchema = False, verbose_func = globalVerbose, verbose = False):
 		"""
 		Return CSV output of self.dictout (default) or a similar dictionary object.
 		Print this object's schema if asked to do so (default to no).
 		"""
 		verbose_func(verbose, dictInput)
-		if inputChk(dictInput, tableSchema):
+		if inputChk(dictInput, validSchema):
 			allVals = None
-			for key in tableSchema:
+			for key in validSchema:
 				allVals.append(dictInput[key])
 			if len(allVals) == 13:
 				if printSchema:
-					return(genCSVfunc(tableSchema) + '\n' + genCSVfunc(allVals))
+					return(genCSVfunc(validSchema) + '\n' + genCSVfunc(allVals))
 				else:
 					return(genCSVfunc(allVals))
 			else:
 				# fail and return none if fewer than 13 output values found, as this is indicative of invalid input.
-				raise ValueError(' : fewer than required number of output values found with standard global tableSchema on method CSVOut.  You are likely passing a miskeyed dictionary.')				
+				raise ValueError(' : fewer than required number of output values found with standard global validSchema on method CSVOut.  You are likely passing a miskeyed dictionary.')				
 				return False
 		else:
 			return False
-	def DictOut(self, dictInput = self.dictout, tableSchema = globalSchema, inputChk = globalDictInputChk, verbose_func = globalVerbose, verbose = False):
+	def DictOut(self, dictInput = self.dictout, validSchema, inputChk = globalDictInputChk, verbose_func = globalVerbose, verbose = False):
 		"""
 		Return a dictionary after validation.
 		"""
 		verbose_func(verbose, dictInput)
-		if inputChk(dictInput, tableSchema):
+		if inputChk(dictInput, validSchema):
 			return dictInput
 		else:
 			return False
-	def TupleOut(self, dictInput = self.dictout, tableSchema = globalSchema, inputChk = globalDictInputChk, verbose_func = globalVerbose, verbose = False)
+	def TupleOut(self, validSchema, dictInput = self.dictout, inputChk = globalDictInputChk, verbose_func = globalVerbose, verbose = False)
 		"""
-		Return a tuple based on a dictionary after validation, using provided tableSchema.
+		Return a tuple based on a dictionary after validation, using provided validSchema.
 		"""
 		verbose_func(verbose, dictInput)
-		if inputChk(dictInput, tableSchema):
+		if inputChk(dictInput, validSchema):
 			prelist = []
-			for elem in tableSchema:
+			for elem in validSchema:
 				prelist.append(dictInput[elem])
 			return tuple(prelist)
 		else:
 			return False
 
-class DSPerfDB:
-	tableSchema = 'system_name', 
-	def __init__(self, location=':memory:')
+class DSPerfDB_SQLite3:
+	"""
+	Initializes an SQLite3 database.  Provides a standard interface for table creation and data extraction
+	without needing to manually write SQL (although that is an option).
+	"""
+	def __init__(self, location = ':memory:', tableName = )
+		"""
+		Construct, connect, provide a cursor object.  Default database location is in memory, although this really
+		is arbitrary.
+		"""
 		self.con = sqlite3.connect(location)
 		self.cur = self.con.cursor()
-		# create table.
 		return None
+	def CreateTable(self, tableName, tableSchemaDict, verbose = False)
+		"""
+		Create a table using provided schema.  Using the schema class provided up above is really a smart idea here,
+		although it is hardly a requirement.
+		"""
+		if isinstance(tableSchemaDict, dict):
+			ColCt = 0
+			ColTot = len(tableSchemaDict)
+			iString = None
+			for i in tableSchemaDict:
+				ColCt += 1
+				if ColCt == 1:
+					iString = 'CREATE TABLE ' + str(tableName) + ' (' + str(i) + ' ' + str(tableSchemaDict[i])
+				elif ColCt > 1:
+					iString = iString + ', ' + str(i) + ' ' + str(tableSchemaDict[i])
+			iString = iString + ')'
+			if verbose:
+				print(str(iString))
+			self.cur.execute(iString)
+			self.con.commit()
+			return True
+		else:
+			raise ValueError(' : invalid input for schema - requires type dict')
+			return False
+	def ListTables(self):
+		return self.cur.execute('SELECT name FROM sqlite_master WHERE type=\'table\'')
+	def ListDateRange(self, table, datecol):
+	def SelectFromTable(self, table, datemin, datemax
 	def Close(self):
 		self.con.close()
