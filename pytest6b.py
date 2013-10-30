3d9e0bedbb157de6370504ee89fcaf30df13ee2c
evan.johnson <evan.johnson@faurecia.com>
2013-10-30 16:07:51 -0400

    Continued refinement of stat analysis, heading towards a working application.

--- pytest6b.py
+++ pytest6b.py
@@ -89,68 +89,102 @@ class Analyze:
 	A toolbox that provides some useful analytics (e.g. standard deviation).
 	'''
 	SDevCalc = std	# initialize for standard deviation
-	MedCalc = median
+	MedCalc = median	# initialize for median.
 	def __init__(self):
 		'''
 		Nothing is really going on here.
 		'''
 		return None
-	def CalcStd(self, input_dict)
+	def CalcStd(inlist):
 		'''
-		Accepts a dictionary with keys (probably dates/times) and values.
-		Returns the dictionary with the values now tuples - the values themselves, and their
-		deviance from the mean.
+		Accepts a list of tuples with keys (probably dates/times) and values.
+		Returns this same list of tuples with two values appended: the input value's
+		relative deviation from the median in terms of (positive or negative)
+		deviations from sigma, and an integer result of this same number.
 		'''
 		input_arr = []
-		output_dict = {}
-		for rawval in input_dict.values():
-			input_arr.append(rawval)
+		for i in inlist:
+			if isinstance(i, tuple):
+				input_arr.append(i[1])
+			else:
+				raise ValueError(' : no tuple found where tuple expected.')
+				return False
+		# calculate standard deviation and median
 		med = MedCalc(input_arr)
 		sd = SDevCalc(input_arr)
-		inmax = max(input_arr)
-		inmin = min(input_arr)
-		
-		sdMap = {}
-		sdMap[0] = med
-		sdMap[-1] = ( med, (med - sd) )
-		sdMap[1] = ( med, (med + sd ) )
-		oi = sdMap[-1]
-		posi = -1
-		while oi >= inmin:
-			posi = posi - 1
-			sdMap[posi] = ( oi, ( oi - sd ) )
-			oi = oi - sd
-		oi = sdMap[1]
-		posi = 1
-		while oi <= inmin:
-			posi = posi + 1
-			sdMap[posi] = ( oi, ( oi + sd ) )
-			oi = oi + sd
-		while len(mo) > 0:
-			worker = input_dict.popitem()
-				if worker[1] == med:
-					output_dict[worker[0]] = ( worker[1], 0 )					
-				elif worker[1] < med:
-					
-
-		
-		sdMap = {}
-		sdMap[0] = med
-		oi = med
-		posi = 0
-		while oi >= inmin:
-			oi = (med - sd)
-			posi = posi - 1
-			sdMap[posi] = oi
-		oi = med
-		posi = 0
-		while oi <= inmax:
-			oi = (med + sd)
-			posi = posi + 1
-			sdMap[posi] = oi
-		while len(mo) > 0:
-			worker = mo.popitem()
-			if worker[1] < med:
-			
+		# bound for sigma max and min
+		# sigma min:
+		sigmaMin = med
+		sigma = 0
+		sMap = {}
+		while sigmaMin > min(input_arr):
+			uBsigmaMin = sigmaMin
+			sigma += 1
+			sigmaMin = med - ( sd * sigma )
+			sMap[(sigma * -1)] = ( sigmaMin, uBsigmaMin )
+		# sigma max (similar, but non negative)
+		sigmaMax = med
+		sigma = 0
+		while sigmaMax < max(input_arr):
+			lBsigmaMax = sigmaMax
+			sigma += 1
+			sigmaMax = med + ( sd * sigma )
+			sMap[sigma] = ( lBsigmaMax, sigmaMax )
+		# calculate deviance (in terms of multiples of sigma) for each value in inlist
+		# we'll be tupling this up with the input and pushing it to an output list.
+		outList = []
+		while len(inlist) > 0:
+			worker = inlist.pop()
+			if ( worker[1] == med ):
+				varSig = 0
+				sigmaO = 0
 			else:
-				
+				for sigmaInt in sMap.keys():
+					range = sMap[sigmaInt]
+					# is it in range?
+					if ( worker[1] >= range[0] ) and ( worker[1] < range[1] ):
+						if ( sigmaInt < 0 ):
+							varSig = ( ( range[1] - worker[1] ) / sd ) + sigmaInt
+							sigmaO = sigmaInt
+						elif ( sigmaInt > 0 ):
+							varSig = ( ( worker[1] - range[1] ) / sd ) + sigmaInt
+							sigmaO = sigmaInt
+						break
+					else:
+						continue
+			outList.append( (worker[0], worker[1], varSig, sigmaO) )
+		return outList
+	def StdOnly(inlist):
+		'''
+		Returns only the standard deviation as a float - no list, nothing - just the measure
+		of variance.  Accepts a standard list (no nesting).
+		'''
+		return SDevCalc(inlist)
+
+'''
+The following is specific for the Exchange arrays ( e.g. file schemas
+used will differ).
+'''
+
+schemMap = {}
+schemMap[1] = ( 'SYSTEM_NAME', 'TEXT' )
+schemMap[2] = ( 'DATE', 'DATE' )
+schemMap[3] = ( 'TIME', 'TIME' )
+schemMap[4] = ( 'DAY_OF_WEEK', 'TEXT' )
+schemMap[5] = ( 'CONT-SYS-LD_NAME', 'TEXT' )
+schemMap[6] = ( 'TOTAL_IOPS', 'FLOAT' )
+schemMap[7] = ( 'READ_PCT', 'FLOAT' )
+schemMap[8] = ( 'CACHE_HIT_PCT', 'FLOAT' )
+schemMap[9] = ( 'CURRENT_KBPS', 'FLOAT' )
+schemMap[10] = ( 'MAX_KBPS', 'FLOAT' )
+schemMap[11] = ( 'CURRENT_IOPS', 'FLOAT' )
+schemMap[12] = ( 'MAXIMUM_IOPS', 'FLOAT' )
+
+ds47xSchem = Schema(schemMap)
+
+aDBcon = sqlite3.connect(':memory:')
+aDBcur = aDBcon.cursor()
+
+ifile = None
+while ifile != 99:		
+	ifile = input('Please enter the fully qualified path to an input file from IBM DS4700 arrays USUS1SAN1010 or USUS1SAN1020 :')
